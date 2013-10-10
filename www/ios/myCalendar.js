d33768c29fb243a9497561a98e06649bcb6cbbb5
duchung852002 <duchung.nguyen@smsmt.com>
2013-10-10 17:02:50 +0700

    - Added ios plugin

---
+++ www/ios/myCalendar.js
@@ -0,0 +1,18 @@
+var calendarPlugin = {
+    createEvent: function(title, location, notes, startDateTime, endDateTime, reminderMins, successCallback, errorCallback) {
+        cordova.exec(
+			successCallback, // success callback function
+			errorCallback, // error callback function
+			'Calendar', // mapped to our native Java class called "CalendarPlugin"
+			'cordovaAddNewEventToCalendar', // with this action name
+			[{                  // and this array of custom arguments to create our entry
+				"title": title,
+				"description": notes,
+				"eventLocation": location,
+				"startDateTime": startDateTime,
+				"endDateTime": endDateTime,
+				"remiderMins": reminderMins
+			}]
+		);
+    }
+}
